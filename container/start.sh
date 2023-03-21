d58ab0320cc45d9c4e7c0a9e42a8b0755224bc9b
Fabrizio Fiorucci <fiorucci@oasi.asti.it>
2023-03-21 14:54:16 +0100

    Start script update

--- container/start.sh
+++ container/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#nginx -g "daemon off;"
 nginx
 sleep 2
 
@@ -31,5 +30,8 @@ if [[ "$NAP_WAF_PRECOMPILED_POLICIES" == "true" ]]; then
    PARM="${PARM} --nginx-app-protect-precompiled-publication"
 fi
 
-nginx-agent $PARM
+if [[ "$ACM_DEVPORTAL" == "true" ]]; then
+   nginx-devportal server &
+fi
 
+nginx-agent $PARM
